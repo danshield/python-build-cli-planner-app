bd3f0539de72bf7551955b8c842d927dca9184cc
Dan Shield <Dan.Shield@allstate.com>
2021-01-14 12:58:48 -0600

    Stil going

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,8 +1,28 @@
 from abc import ABCMeta, abstractmethod
 from collections.abc import Iterable 
+from abc import ABC
+from dateutil.parser import parse
+from datetime import datetime
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
 
     @abstractmethod()
     def is_due():
         pass
+
+class DeadlinedReminder(ABC, Iterable):
+
+    @abstractmethod
+    def is_due():
+        pass
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text, date);
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+
+    def is_due():
+        return self.date <= datetime.now()
+
+    def __iter__():
+        iter([self.text, self.date.isoformat()])
