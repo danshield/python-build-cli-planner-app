458b025cd23812562542349292a5d3345db91c01
Dan Shield <Dan.Shield@allstate.com>
2021-01-14 14:04:09 -0600

    Tired

--- src/database.py
+++ src/database.py
@@ -1,5 +1,4 @@
 import csv
-from src.reminder import PoliteReminder
 from src.deadlined_reminders import DateReminder
 
 def list_reminders():
@@ -14,7 +13,10 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text, date):
+def add_reminder(text, date, ReminderClass):
+    if not issubclass(ReminderClass, DateReminder):
+        raise "Invalid Reminder Class"
+
     reminder = DateReminder(text, date)
 
     with open('reminders.csv', 'a+', newline='\n') as file:
